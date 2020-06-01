894a0840c707554a31efc8d378119ebbf38249e6
dankbot <66181321+dankbot@users.noreply.github.com>
2020-06-01 15:09:24 +0200

    mostly complete the webdriver downloader?

--- webdriver_downloader.py
+++ webdriver_downloader.py
@@ -1,6 +1,13 @@
 import os
 import platform
+import shutil
 import subprocess
+import sys
+import zipfile
+
+from PyQt5.QtCore import QUrl, QFile, QIODevice, pyqtSignal
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PyQt5.QtWidgets import QProgressDialog, QApplication, QMessageBox
 
 if platform.system() == "Windows":
     from win32api import GetFileVersionInfo, LOWORD, HIWORD
@@ -32,13 +39,11 @@ def get_dirs():
         ret += ["/opt/google/chrome", "/opt/chromium.org/chromium"]
     return ret
 
-
 def get_chrome_exe_names():
     if platform.system() == "Windows":
         return ["chrome.exe", "chromium.exe"]
     else:
         return ["chrome", "chromium"]
-
 
 def find_chrome():
     names = get_chrome_exe_names()
@@ -47,7 +52,6 @@ def find_chrome():
             if os.path.exists(os.path.join(d, n)):
                 return os.path.join(d, n)
     return None
-
 
 def get_chrome_version():
     c = find_chrome()
@@ -58,5 +62,124 @@ def get_chrome_version():
     else:
         return subprocess.check_output([c, "--product-version"]).decode().strip().split(".")
 
+def get_webdriver_path(version):
+    v = version[0]
+    if platform.system() == "Windows":
+        return f"chromedriver_{v}.exe"
+    else:
+        return f"chromedriver_{v}"
 
-print(get_chrome_version())
+def check_webdriver_version(version):
+    p = get_webdriver_path(version)
+    return os.path.exists(p)
+
+def get_webdriver_version_url(chrome_version):
+    return f"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{chrome_version[0]}"
+
+def get_webdriver_download_url(webdriver_version):
+    if platform.system() == "Windows":
+        plat = "win32"
+    elif platform.system() == "Linux":
+        plat = "linux64"
+    elif platform.system() == "Darwin":
+        plat = "mac64"
+    else:
+        return None
+    return f"https://chromedriver.storage.googleapis.com/{webdriver_version}/chromedriver_{plat}.zip"
+
+
+class QWebDriverDownloader:
+    completed = pyqtSignal(str)
+
+    def __init__(self, parent):
+        self.success = False
+
+        self.net = QNetworkAccessManager()
+
+        self.dialog = QProgressDialog(parent)
+        self.dialog.setWindowTitle("Chrome Web Driver Download")
+        self.dialog.canceled.disconnect()
+        self.dialog.canceled.connect(self.cancel)
+
+        self.dialog.setLabelText("Getting the Web Driver version")
+        self.dialog.setMinimum(0)
+        self.dialog.setMaximum(0)
+        self.dialog.setValue(0)
+        self.dialog.show()
+
+        cv = get_chrome_version()
+        self.webdriver_path = get_webdriver_path(cv)
+        url = get_webdriver_version_url(cv)
+        print(f"Getting Web Driver version from: {url}")
+        req = QNetworkRequest(QUrl(url))
+        self.version_rep = self.net.get(req)
+        self.version_rep.finished.connect(self.on_version_fetched)
+        self.download_rep = None
+        self.download_file = None
+
+    def cancel(self):
+        self.version_rep.abort()
+        if self.download_rep is not None:
+            self.download_rep.abort()
+
+    def report_error(self, req):
+        print(f"Download failed")
+
+        if self.download_file is not None:
+            self.download_file.close()
+            self.download_file.remove()
+
+        QMessageBox.information(self.dialog, "Web Driver Download", f"Failed to download Web Driver: {req.errorString()}")
+        self.dialog.close()
+        self.completed.emit()
+
+    def on_version_fetched(self):
+        if self.version_rep.error() != QNetworkReply.NoError:
+            self.report_error(self.version_rep)
+            return
+
+        ver = bytes(self.version_rep.readAll()).decode().strip()
+        print(f"Web Driver version: {ver}")
+
+        url = get_webdriver_download_url(ver)
+        print(f"Getting Web Driver from: {url}")
+
+        req = QNetworkRequest(QUrl(url))
+        self.download_file = QFile(self.webdriver_path + ".zip.tmp")
+        self.download_file.open(QIODevice.WriteOnly)
+        self.download_rep = self.net.get(req)
+        self.download_rep.finished.connect(self.on_download_completed)
+        self.download_rep.downloadProgress.connect(self.on_download_progress)
+        self.download_rep.readyRead.connect(self.on_read_ready)
+
+    def on_download_progress(self, cur, total):
+        print(f"Download progress: {cur}/{total}")
+        self.dialog.setMaximum(total)
+        self.dialog.setValue(cur)
+
+    def on_download_completed(self):
+        if self.download_rep.error() != QNetworkReply.NoError:
+            self.report_error(self.download_rep)
+            return
+        print(f"Download completed")
+        self.download_file.write(self.download_rep.readAll())
+        self.download_file.close()
+
+        print(f"Unzipping")
+        with zipfile.ZipFile(self.webdriver_path + ".zip.tmp", 'r') as z:
+            with z.open("chromedriver") as src, open(self.webdriver_path, "wb") as dest:
+                shutil.copyfileobj(src, dest)
+        self.download_file.remove()
+
+        print(f"Everything completed")
+        self.success = True
+        self.dialog.close()
+        self.completed.emit()
+
+    def on_read_ready(self):
+        self.download_file.write(self.download_rep.readAll())
+
+
+app = QApplication([])
+d = QWebDriverDownloader(None)
+sys.exit(app.exec_())
